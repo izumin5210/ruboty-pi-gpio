2f9b655cda6eb8b6bc1cf1c34c54e3e65f4bf7cb
izumin5210 <masayuki@izumin.info>
2015-08-29 04:05:14 +0900

    Add Ruboty::PiGpio::Actions::Read

---
+++ lib/ruboty/pi_gpio/actions/read.rb
@@ -0,0 +1,20 @@
+module Ruboty
+  module PiGpio
+    module Actions
+      class Read < Base
+        def call
+          message.reply("GPIO#{pin} is #{value}")
+        rescue
+          message.reply("Failed to get GPIO#{pin} value")
+        end
+
+        private
+
+        def value
+          File.read("/sys/class/gpio/gpio#{pin}/value")
+        end
+      end
+    end
+  end
+end
+
