c27c23203a9e25383c6fd43f4e99147d53c4e71a
izumin5210 <masayuki@izumin.info>
2015-08-29 04:15:21 +0900

    Unexport GPIOs when the script is finished

--- lib/ruboty/pi_gpio.rb
+++ lib/ruboty/pi_gpio.rb
@@ -5,3 +5,9 @@ require "ruboty/pi_gpio/actions/mode"
 require "ruboty/pi_gpio/actions/write"
 require "ruboty/pi_gpio/actions/read"
 require "ruboty/pi_gpio/actions/unexport"
+
+END {
+  Dir.glob('/sys/class/gpio/gpio*').each do |gpio|
+    open('/sys/classs/gpio/unexport', 'w') { |f| f.write(gpio) }
+  end
+}
